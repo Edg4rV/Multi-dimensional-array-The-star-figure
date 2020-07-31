d3261b178f7bb47473e87dc42cf458a041874aa4
Edg4rV <vardedgar@mail.ru>
2020-08-01 01:58:34 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int len = scanner.nextInt();
+        String[][] matrix = new String[len][len];
+        int y = 0;
+        for (int i = 0; i < len; i++) {
+            y++;
+            for(int j = 0; j < len; j++) {
+                if (i == j || i == len / 2 || j == len - y) {
+                    matrix[i][j] = "*";
+                } else if (j == len / 2) {
+                    for (int k = 0; k < len; k++) {
+                        matrix[k][len / 2] = "*";
+                    }
+                } else {
+                    matrix[i][j] = ".";
+                }
+            }
+        }
+
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = len - 1; j >= 0; j--) {
+                if (i == j) {
+                    matrix[i][j] = "*";
+                }
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
